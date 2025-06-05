1d8b965fe58b39f0e27034642c61155ffbcdcc22
teesha08 <teeshasaini08@gmail.com>
2025-06-05 18:57:22 +0530

    Final

---
+++ minMax.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
+        long totalSum = 0;
+        
+        for(int number : arr){
+            totalSum += number;
+            
+            if(number < minValue){
+                minValue = number;
+            }
+            
+            if(number > maxValue){
+                maxValue = number;
+            }
+        }
+        
+        long minSum = totalSum - maxValue;
+        long maxSum = totalSum - minValue;
+        
+        System.out.println(minSum + " " + maxSum);
+    }
+
+}
+
+public class Solution1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
--- plusMin.java
+++ plusMin.java
@@ -6,53 +6,56 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-class Result2 {
+class Result {
 
     /*
-     * Complete the 'miniMaxSum' function below.
+     * Complete the 'plusMinus' function below.
      *
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
-    public static void miniMaxSum(List<Integer> arr) {
-        int minValue = Integer.MAX_VALUE;
-        int maxValue = Integer.MIN_VALUE;
-        long totalSum = 0;
+    public static void plusMinus(List<Integer> arr) {
+        int positiveInt = 0;
+        int negativeInt = 0;
+        int zero = 0;
+        int n = arr.size();
         
         for(int number : arr){
-            totalSum += number;
-            
-            if(number < minValue){
-                minValue = number;
+            if(number>0){
+                positiveInt++;
             }
-            
-            if(number > maxValue){
-                maxValue = number;
+            else if(number<0){
+                negativeInt++;
+            }
+            else{
+                zero++;
             }
         }
         
-        long minSum = totalSum - maxValue;
-        long maxSum = totalSum - minValue;
+        System.out.printf("%.6f%n", (double) positiveInt / n);
+        System.out.printf("%.6f%n", (double) negativeInt / n);
+        System.out.printf("%.6f%n", (double) zero / n);
         
-        System.out.println(minSum + " " + maxSum);
     }
 
 }
 
-public class Solution {
+public class plusMin {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
         String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
         List<Integer> arr = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < n; i++) {
             int arrItem = Integer.parseInt(arrTemp[i]);
             arr.add(arrItem);
         }
 
-        Result.miniMaxSum(arr);
+        Result.plusMinus(arr);
 
         bufferedReader.close();
     }
