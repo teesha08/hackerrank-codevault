7361f21dba1bf93fccaa9a6f81ea2b122bc61ca5
teesha08 <teeshasaini08@gmail.com>
2025-06-10 17:39:28 +0530

    Final

---
+++ viralAdvertising.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+        int recipient = 5;
+        int numberOfLikes = 0;
+        
+         for(int i = 0 ; i < n ; i++) {
+            numberOfLikes += recipient/2;
+            recipient = (recipient/2) * 3;
+         }
+         
+         return numberOfLikes;
+    }
+
+}
+
+public class viralAdvertising {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
