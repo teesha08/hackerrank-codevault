8d7b49c9c863d1aa1e08cf59978db12d8ca979a0
teesha08 <teeshasaini08@gmail.com>
2025-06-15 23:24:17 +0530

    Final

---
+++ equalizeArrayProblem.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equalizeArray(List<Integer> arr) {
+        int[] frequency = new int[101];
+        
+        for(int i = 0 ; i < arr.size() ; i++) {
+            int num = arr.get(i);
+            frequency[num]++;
+        }
+        
+        int max = 0;
+        for(int i : arr){
+            if(frequency[i] > max) {
+                max = frequency[i];
+            }
+        }
+        
+        return arr.size() - max;
+        
+    }
+
+}
+
+public class equalizeArrayProblem {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
