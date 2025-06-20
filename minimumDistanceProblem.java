07b6687c079feacd84e04a2c06b848ba72b2ca51
teesha08 <teeshasaini08@gmail.com>
2025-06-20 19:59:15 +0530

    Final

---
+++ minimumDistanceProblem.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<Integer> a) {
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = 0 ; i < a.size() ; i++) {
+            for(int j = i + 1 ; j < a.size() ; j++) {
+                if(a.get(i).equals(a.get(j))) {
+                    min = Math.min(min, Math.abs(i - j));
+                }
+            }
+        }
+        
+        return (min == Integer.MAX_VALUE) ? -1 : min;
+    }
+
+}
+
+public class minimumDistanceProblem {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
