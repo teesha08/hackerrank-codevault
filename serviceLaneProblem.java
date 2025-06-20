ac0ff6036064ae2dbe9319549adee989b35d41b4
teesha08 <teeshasaini08@gmail.com>
2025-06-20 21:08:33 +0530

    Final

---
+++ serviceLaneProblem.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'serviceLane' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY cases
+     */
+
+    public static List<Integer> serviceLane(int n, List<Integer> width, List<List<Integer>> cases) {
+        List<Integer> result = new ArrayList<>();
+        
+        for(List<Integer> currentCase : cases) {
+            int start = currentCase.get(0);
+            int end = currentCase.get(1);
+            
+            int min = Integer.MAX_VALUE;
+            for(int i = start ; i <= end ; i++) {
+                min = Math.min(min, width.get(i));
+            }
+            
+            result.add(min);
+        }
+        
+        return result;
+    }
+
+}
+
+public class serviceLaneProblem {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> width = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<List<Integer>> cases = new ArrayList<>();
+
+        IntStream.range(0, t).forEach(i -> {
+            try {
+                cases.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.serviceLane(n, width, cases);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
